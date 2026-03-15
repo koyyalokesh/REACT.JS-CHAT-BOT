ece5cb51b6a10a5a6987e75b571abc78d636f8ce
Lokesh Koyya <140862301+koyyalokesh@users.noreply.github.com>
2026-03-15 23:14:15 +0530

    Update chatbot.js

--- src/config/chatbot.js
+++ src/config/chatbot.js
@@ -11,7 +11,7 @@ import {
     HarmBlockThreshold,
   } from "@google/generative-ai";
   
-  const apiKey = "AIzaSyDRliXYiUicJV94GycT9n-JN3BwWm_MsXk";
+  const apiKey = "";
   const genAI = new GoogleGenerativeAI(apiKey);
   
   const model = genAI.getGenerativeModel({
